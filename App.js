fab78d18499e1ca7cc61c34b7878aa4748066901
Ravindersharma24 <sharma111nik@gmail.com>
2023-05-27 13:49:51 +0530

    project setup

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const App = () => {
+  return (
+    <Text style={{color:'red'}}>JaiGuruDev :- Please be vegetarian</Text>
+  )
+}
+
+export default App
