2526907175772b9efbb903f121ec0c081e078b92
Ravindersharma24 <sharma111nik@gmail.com>
2023-05-28 18:11:24 +0530

    fetching product api data and display in flatlist

--- src/screens/tabs/Home.js
+++ src/screens/tabs/Home.js
@@ -1,20 +1,94 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Dimensions, FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import React,{useEffect, useState} from 'react'
 import Header from '../../common/Header'
 import {useNavigation} from '@react-navigation/native'
 
 const Home = () => {
     const navigation = useNavigation()
+    const [products,setProducts] = useState();
+    // fetching products data from api
+    useEffect(() => {
+      getProducts();
+    }, [])
+
+    const getProducts = ()=>{
+      fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+    }
+    
   return (
-    <Header 
+    <View style={styles.container}>
+      <Header 
         leftIcon={require("../../images/menu.png")}
         rightIcon={require("../../images/cart.png")}
         title={'Grocerry App'}
         onClickLeftIcon={()=>{navigation.openDrawer()}}
-    />
+        />
+
+        <FlatList
+          data={products}
+          renderItem={({item,index})=>{
+            return(
+              <View style={styles.productItem}>
+                <Image source={{uri:item.image}} style={styles.itemImage}/>
+                <View>
+                  <Text style={styles.name}>
+                    {item.title.length > 25
+                      ? item.title.substring(0,25) + '...'
+                      : item.title
+                    }
+                  </Text>
+                  <Text style={styles.desc}>
+                    {item.description.length > 30
+                      ? item.description.substring(0,30) + '...'
+                      : item.description
+                    }
+                  </Text>
+                  <Text style={styles.price}>
+                      $ {item.price}
+                  </Text>
+                </View>
+              </View>
+            )
+          }}
+        />
+    </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  productItem:{
+    width:Dimensions.get('window').width,
+    height:100,
+    marginTop:10,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    flexDirection:'row'
+  },
+  itemImage:{
+    width:100,
+    height:100,
+  },
+  name:{
+    fontSize:18,
+    fontWeight:600,
+    marginLeft:20,
+  },
+  desc:{
+    marginLeft:20,
+  },
+  price:{
+    color:'green',
+    fontSize:18,
+    fontWeight:600,
+    marginLeft:20,
+    marginTop:20,
+  }
+
+})
